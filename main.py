0690a95530a6788b5fcbf50b068d6d86d583e2fb
Ivanka1106 <pivanna@me.com>
2024-09-29 19:57:26 +0200

    operator_match

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello World')
+point = (1, 0)
+
+match point:
+    case (0, 0):
+        print("Точка в центрі координат")  
+    case (0, y):
+        print(f"Точка лежить на осі Y: y={y}")  
+    case (x, 0):
+        print(f"Точка лежить на осі X: x={x}") 
+    case (x, y):
+        print(f"Точка має координати:  x={x}, y={y}") 
+    case _:
+        print("Це не точка")
